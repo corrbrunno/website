dee9f0c067361e44724090cfbe0f453c052a5ba7
GT <correabruno43@gmail.com>
2026-01-26 18:51:58 -0300

    style: Spacing more consistent, responsiviness on mobile

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -6,15 +6,15 @@
 	const isServerError = page.status >= 500;
 </script>
 
-<section class="flex h-full w-full flex-col content-center items-center text-center justify-center p-3">
+<section class="flex h-full w-full flex-col content-center items-center  justify-center p-3">
 	<div class="mb-1 text-center">
 		<h1 class="text-3xl"><strong>{page.status}</strong>: {page.error?.message}</h1>
 	</div>
-	<p class="text-sm mb-1">Não foi possível carregar esta págia.</p>
-	<div class="flex flex-col items-center justify-center text-center md:flex-row">
-		<Info class="mt-3 size-4 shrink-0 md:m-0 md:mr-2" />
-
-		<p class="text-sm">
+	<p class="text-sm mb-4">Não foi possível carregar esta págia.</p>
+	<div class="flex flex-row justify-center items-center content-center flex-0">
+		
+		<Info class="min-w-4 max-w-4 mr-2 mb-auto -translate-y-1" />
+		<p >
 			{#if isServerError}
 				Sinto muito, houve algum erro no servidor.
 			{:else}
@@ -23,11 +23,12 @@
 			<Button
 				href={`https://developer.mozilla.org/pt-BR/docs/Web/HTTP/Reference/Status/${page.status}`}
 				variant="link"
-				class="inline-flex h-auto p-0 align-baseline"
+				class="inline p-0"
 			>
 				Saiba mais clicando aqui.
 			</Button>
 		</p>
-	</div>
-	<Button variant="secondary" href="/" class="mt-4">Voltar ao início</Button>
+</div>
+
+	<Button variant="secondary" href="/" class="mt-5">Voltar ao início</Button>
 </section>
