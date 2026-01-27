89a4a4841d81c3f6cbcbb4d6fc680ce9d957d798
corrbrunno <correabruno43@gmail.com>
2026-01-27 08:28:32 -0300

    implemented i18n on homepage

--- src/routes/(home)/impact-banner.svelte
+++ src/routes/(home)/impact-banner.svelte
@@ -1,20 +1,24 @@
+<script>
+    import * as m from '$lib/paraglide/messages.js';
+</script>
+
+
 <section class="flex w-full overflow-x-clip flex-col items-center justify-center">
 	<section
 		id="impact"
 		class="max-w-content-width relative flex flex-col items-center justify-between gap-2 p-4 before:hidden after:hidden md:flex-row md:before:block md:after:block"
 	>
 		<h1 class="select-none flex-0 relative pb-2 text-left text-2xl font-bold md:text-5xl">
-			Soluções incontáveis, foco em <mark class="text-primary bg-transparent">resultados</mark>.
+			{m.home_impact_title()} <mark class="text-primary bg-transparent">{m.home_impact_results_label()}</mark>.
 		</h1>
 		<p class="w-full max-w-lg px-5 text-justify whitespace-pre-line">
-			Sou Bruno e transformo desafios digitais em processos fluidos e inteligentes.
-			 Movido pela inovação constante, uso este espaço para criar, apresentar e ensinar. Explore meus projetos ou mergulhe no blog para entender os bastidores dessa jornada.
+			{m.home_impact_description()}
 		</p>
 		<h1
 			style="anchor-name: --slogan;"
 			class="select-none flex-0 relative w-fit items-center py-4 text-center text-xl font-bold md:pt-0 md:text-right md:text-5xl"
 		>
-			Rápido, prático & <mark class="text-primary bg-transparent">inteligente</mark>
+			{m.home_impact_slogan()} <mark class="text-primary bg-transparent">{m.home_impact_smart_label()}</mark>
 		</h1>
 	</section>
 </section>
