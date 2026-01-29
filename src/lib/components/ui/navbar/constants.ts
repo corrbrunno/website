b637e0d73c4561341d0c2ea7baf0507b9a4e069c
corrbrunno <correabruno43@gmail.com>
2026-01-29 09:45:24 -0300

    feat: added translations for sidebar, added service section

--- src/lib/components/ui/navbar/constants.ts
+++ src/lib/components/ui/navbar/constants.ts
@@ -1,4 +1,4 @@
-import { HouseIcon, SquareUserRound, NotebookPen } from "@lucide/svelte";
+import { HouseIcon, SquareUserRound, NotebookPen, Bot, Book, MonitorCloud } from "@lucide/svelte";
 import * as m from '$lib/paraglide/messages';
 
 export const NAVEGATION_BUTTONS = [
@@ -6,3 +6,13 @@ export const NAVEGATION_BUTTONS = [
     { slug: m.nav_about(), url: '/#about', icon: SquareUserRound },
     { slug: m.nav_blog(), url: '/blog', icon: NotebookPen },
 ];
+
+export const SERVICE_PAGES = [
+    {slug: m.nav_pricing_automation, url: "/pricing/automations", icon: Bot},
+    {slug: m.nav_pricing_class, url: "/pricing/classes", icon: Book},
+    {slug: m.nav_pricing_website, url: "/pricing/websites", icon: MonitorCloud}
+]
+
+export const SUPPORTED_LANGUAGES = [
+    { lang: m.language_portuguese, locale: "pt-br", emoji: ``}
+]
--- src/lib/components/ui/navbar/nav-sidebar.svelte
+++ src/lib/components/ui/navbar/nav-sidebar.svelte
@@ -2,15 +2,18 @@
 
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	import { Button } from '../button';
-	import { NAVEGATION_BUTTONS } from './constants';
+	import { NAVEGATION_BUTTONS, SERVICE_PAGES } from './constants';
+	import * as m from '$lib/paraglide/messages';
+	import NavbarThemeToggle from './navbar-theme-toggle.svelte';
 
 </script>
 
 
 <Sidebar.Root viewTransitionName="main-sidebar" variant="floating" side="right">
-	<Sidebar.Content>
+	<Sidebar.Content class="p-5">
+		<Sidebar.Header>Bruno Corrêa</Sidebar.Header>
 		<Sidebar.Group>
-			<Sidebar.GroupLabel >My Site:</Sidebar.GroupLabel>
+			<Sidebar.GroupLabel >{m.nav_general_section()}</Sidebar.GroupLabel>
 			<Sidebar.GroupContent>
 				<Sidebar.Menu>
 					{#each NAVEGATION_BUTTONS as button (button.slug)}
@@ -29,6 +32,29 @@
 				</Sidebar.Menu>
 			</Sidebar.GroupContent>
 		</Sidebar.Group>
+		<Sidebar.Group>
+			<Sidebar.GroupLabel >{m.nav_service_section()}</Sidebar.GroupLabel>
+			<Sidebar.GroupContent>
+				<Sidebar.Menu>
+					{#each SERVICE_PAGES as button (button.slug)}
+						<Sidebar.MenuItem>
+							<Sidebar.MenuButton class="flex justify-start
+							">
+								{#snippet child({ props })}
+									<Button variant="link" href={button.url} {...props}>
+										<button.icon />
+										<span>{button.slug()}</span>
+									</Button>
+								{/snippet}
+							</Sidebar.MenuButton>
+						</Sidebar.MenuItem>
+					{/each}
+				</Sidebar.Menu>
+			</Sidebar.GroupContent>
+		</Sidebar.Group>
 	</Sidebar.Content>
+	<Sidebar.Footer class="p-5">
+		<NavbarThemeToggle/>
+	</Sidebar.Footer>
 </Sidebar.Root>
 
