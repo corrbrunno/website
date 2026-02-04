4e48db518a9f89590213a4fc5d19444effd044b1
GT <correabruno43@gmail.com>
2026-02-04 20:16:50 -0300

    Feat(pricing): Added highlight badge for pricing-card component

--- src/routes/pricing/pricing-card.svelte
+++ src/routes/pricing/pricing-card.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import Button from '$lib/components/ui/button/button.svelte';
+	import Badge from '$lib/components/ui/badge/badge.svelte';
+import Button from '$lib/components/ui/button/button.svelte';
 	import * as Card from '$lib/components/ui/card';
 	import Separator from '$lib/components/ui/separator/separator.svelte';
 	import { getLocale } from '$lib/paraglide/runtime';
@@ -11,30 +12,36 @@
 	export type pricingCardData = {
 		title: () => LocalizedString;
 		paymentFrequency?: () => LocalizedString;
+		highlight?: () => LocalizedString;
 		actionButton: { name: () => LocalizedString; link: string };
 		price: number;
 		featureItems: featureItem[];
 	};
 
-	const { price, paymentFrequency, title, featureItems, actionButton }: pricingCardData = $props();
+	const { price, highlight, paymentFrequency, title, featureItems, actionButton }: pricingCardData = $props();
 </script>
 
-<Card.Root class="h-fit text-pretty w-65">
-	<Card.Header class="flex flex-col items-center">
-		<h2>{title()}</h2>
-		<Separator />
-		<div>
-			<h3 class="text-primary text-4xl font-bold">R$ {Intl.NumberFormat(getLocale()).format(price)}</h3>
-			<p class="-mt-2 text-right text-sm">/{paymentFrequency?.()}</p>
+<div class="flex flex-col gap-2 justify-start">
+	{#if highlight}
+		<Badge>{highlight()}</Badge>
+	{/if}
+	
+	<Card.Root class="h-fit text-pretty w-65">
+		<Card.Header class="flex flex-col items-center">
+			<h2>{title()}</h2>
+			<Separator />
+			<div>
+				<h3 class="text-primary text-4xl font-bold">R$ {Intl.NumberFormat(getLocale()).format(price)}</h3>
+				<p class="-mt-2 text-right text-sm">/{paymentFrequency?.()}</p>
 		</div>
 	</Card.Header>
 	<Card.Content>
 		<ul class="flex flex-col gap-2">
 			{#each featureItems as item}
-				<li class="flex gap-2 text-left text-balance">
-					<item.icon class="size-6 shrink-0"/>
-					<p>{item.feature()}</p>
-				</li>
+			<li class="flex gap-2 text-left text-balance">
+				<item.icon class="size-6 shrink-0"/>
+				<p>{item.feature()}</p>
+			</li>
 			{/each}
 		</ul>
 	</Card.Content>
@@ -42,3 +49,4 @@
 		<Button href={actionButton.link} class="w-full">{actionButton.name()}</Button>
 	</Card.Footer>
 </Card.Root>
+</div>
