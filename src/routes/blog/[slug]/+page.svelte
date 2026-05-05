e273aaf05748932fb64c91f636b0202df518b053
GT <correabruno43@gmail.com>
2026-05-05 13:29:32 -0300

    Fix(viewTransistions): Added view-transitions-name to Content card and control buttons

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -12,27 +12,32 @@
 	<title>{data.metadata.title} | BCB</title>
 	<meta name="description" content={data.metadata.description} />
 </svelte:head>
-<div class="mr-4 ml-4 ">
+<div class="mr-4 ml-4">
 	<section class="max-w-content-width mr-auto ml-auto w-full pt-15">
 		<Widget abbreviate={false} post={data.metadata} />
 	</section>
 
-	<ul class="flex w-full max-w-content-width justify-between mt-4 mb-4 m-auto">
-			<li>
-				<Button href="/blog">
-					<ChevronLeft/>
-					Posts
-				</Button>
-			</li>
-			<li>
-				<Button href={`/blog/random?exclude=${data.metadata.slug}`}>
-					Random
-					<ChevronRight/>
-				</Button>
-			</li>
+	<ul 
+	style="view-transition-name: blog-post-controls;"
+	class="max-w-content-width m-auto mt-4 mb-4 flex w-full justify-between">
+		<li>
+			<Button href="/blog">
+				<ChevronLeft />
+				Posts
+			</Button>
+		</li>
+		<li>
+			<Button href={`/blog/random?exclude=${data.metadata.slug}`}>
+				Random
+				<ChevronRight />
+			</Button>
+		</li>
 	</ul>
 
-	<Card.Root class="max-w-block-width mt-10 mr-auto mb-10 ml-auto w-full p-5">
+	<Card.Root
+		style="view-transition-name: blog-post-content;"
+		class="max-w-block-width mt-10 mr-auto mb-10 ml-auto w-full p-5"
+	>
 		<Card.Content>
 			<article
 				class="prose prose-purple dark:prose-invert lg:prose-lg text-foreground max-w-none justify-center"
